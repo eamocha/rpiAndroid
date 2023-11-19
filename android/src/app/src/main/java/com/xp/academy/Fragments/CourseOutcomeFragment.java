1876a840fe90164864c9003aa2153b22998c6c39
unknown <eamocha@gmail.com>
2023-11-20 01:04:18 +0300

    update files

---
+++ android/src/app/src/main/java/com/xp/academy/Fragments/CourseOutcomeFragment.java
@@ -0,0 +1,51 @@
+package com.xp.academy.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.xp.academy.Adapters.CourseIncludesAdapter;
+import com.xp.academy.Adapters.CourseOutcomesAdapter;
+import com.xp.academy.Models.CourseDetails;
+import com.xp.academy.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CourseOutcomeFragment extends Fragment {
+
+    private CourseDetails mCourseDetails;
+    private RecyclerView mCourseOutcomesRecyclerView;
+    public CourseOutcomeFragment(CourseDetails courseDetails) {
+        // Required empty public constructor
+        this.mCourseDetails = courseDetails;
+    }
+
+    private void init(View view) {
+        mCourseOutcomesRecyclerView = view.findViewById(R.id.courseOutcomeRecyclerView);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.fragment_course_outcome, container, false);
+        init(view);
+        getOutcomesData();
+        return view;
+    }
+
+    private void getOutcomesData() {
+        CourseOutcomesAdapter adapter = new CourseOutcomesAdapter(getActivity(), mCourseDetails.getCourseOutcomes());
+        mCourseOutcomesRecyclerView.setAdapter(adapter);
+        mCourseOutcomesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+}
